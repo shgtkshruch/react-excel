34106f283738bba0481b3d92dfa80c5d95e7a157
shgtkshruch <shgtk.shruch@gmail.com>
2017-04-12 13:44:53 +0900

    Add table body

--- src/Excel.js
+++ src/Excel.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Excel.css';
 
 class Excel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: props.initialData
+    };
+  }
+
   render() {
     return (
       <table>
@@ -12,9 +21,31 @@ class Excel extends Component {
             })}
           </tr>
         </thead>
+        <tbody>
+          {this.state.data.map((row, id) => {
+            return (
+              <tr key={id}>
+                {row.map((cell, id) => {
+                  return <td key={id}>{cell}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
       </table>
     );
   }
+}
+
+Excel.propTypes = {
+  headers: PropTypes.arrayOf(
+    PropTypes.string
+  ),
+  initialData: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.string
+    )
+  )
 }
 
 export default Excel;
